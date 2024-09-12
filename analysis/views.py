f6e96751d3e309f4698344398a6225ccfd1434ba
Amcool <Alphonceomondi273@gmail.com>
2024-09-12 11:27:22 +0300

    Handling callback and access tokens

--- analysis/views.py
+++ analysis/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
 import requests
 from django.conf import settings
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 def index(request):
     return render(request, 'analysis/index.html')
@@ -19,3 +18,25 @@ def spotify_login(request):
     
     url = f"{auth_url}?{'&'.join([f'{key}={value}' for key, value in params.items()])}"
     return redirect(url)
+
+def spotify_callback(request):
+    code = request.GET.get('code')
+    
+    token_url = "https//accounts.spotify/api/token"
+    
+    payload ={
+        "grant_type" : "authorization_code",
+        "code" : code,
+        "redirect_uri" : settings.SPOTIFY_REDIRECT_URI,
+        "client_id" : settings.CLIENT_ID,
+        "client_secret" : settings.CLIENT_SECRET
+    }
+    
+    response = requests.post(token_url, data = payload)
+    token_data = response.json()
+    
+    access_token = token_data.get('access_token')
+    
+    request.session['spotify_token'] = access_token # save token to session
+    
+    return redirect('spotify_profile')
