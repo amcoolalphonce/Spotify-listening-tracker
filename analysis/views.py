35fd07da07f4854ad360f6763c9ad51e71a9b386
Amcool <Alphonceomondi273@gmail.com>
2024-09-12 11:08:21 +0300

    Authentication process

--- analysis/views.py
+++ analysis/views.py
@@ -5,3 +5,17 @@ from django.shortcuts import redirect
 
 def index(request):
     return render(request, 'analysis/index.html')
+
+def spotify_login(request):
+    scope = 'user-read-recently-played user-top-read'
+    auth_url = 'https://accounts.spotify.com/authorize'
+    
+    params = {
+        "client_id" : settings.CLIENT_ID,
+        "response_type" : "code",
+        "redirect_uri" : settings.SPOTIFY_REDIRECT_URI,
+        "scope" : scope,
+    }
+    
+    url = f"{auth_url}?{'&'.join([f'{key}={value}' for key, value in params.items()])}"
+    return redirect(url)
