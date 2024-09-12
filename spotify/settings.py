44f743ea8f200361cf8e43e3e364e2c5c803b745
Amcool <Alphonceomondi273@gmail.com>
2024-09-12 11:07:52 +0300

    redirect URI

--- spotify/settings.py
+++ spotify/settings.py
@@ -126,4 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CLIENT_ID = config('CLIENT_ID')
 CLIENT_SECRET = config('CLIENT_SECRET')
-SPOTIFY_REDIRECT_URL = 'http://127.0.0.1:8000/callback'
+SPOTIFY_REDIRECT_URI = 'http://127.0.0.1:8000/callback'
