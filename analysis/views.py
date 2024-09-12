c555c1bad173615309e1cb18a61f2fd3655b7687
Amcool <Alphonceomondi273@gmail.com>
2024-09-12 10:53:55 +0300

    installed requests for authentication

--- analysis/views.py
+++ analysis/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import requests
+from django.conf import settings
+from django.shortcuts import redirect
 
 def index(request):
     return render(request, 'analysis/index.html')
