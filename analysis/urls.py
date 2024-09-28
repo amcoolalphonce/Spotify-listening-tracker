a8abf73c8a513832727760214cc7e4fa36484973
Amcool <Alphonceomondi273@gmail.com>
2024-09-28 20:35:19 +0300

    Remove the forward slash from url for callback

--- analysis/urls.py
+++ analysis/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name  = 'index'),
     path('login/', views.spotify_login, name = 'spotify_login'),
-    path('callback/', views.spotify_callback, name = 'spotify_callback'),
+    path('callback', views.spotify_callback, name = 'spotify_callback'),
     path('profile/', views.spotify_profile, name = 'spotify_profile'),
 ]
